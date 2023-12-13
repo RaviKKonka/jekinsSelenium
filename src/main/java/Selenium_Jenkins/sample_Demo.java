0af4fb29f1592d33a52d2dba8346dc9376776ec3
RaviKKonka <ravikonka76@gmail.com>
2023-12-12 21:48:31 -0600

    param

--- src/main/java/Selenium_Jenkins/sample_Demo.java
+++ src/main/java/Selenium_Jenkins/sample_Demo.java
@@ -1,16 +1,24 @@
 	package Selenium_Jenkins;
 	
-	import org.testng.Assert;
-	import org.testng.annotations.Test;
 	import org.openqa.selenium.WebDriver;
 	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.openqa.selenium.edge.EdgeDriver;
+	import org.testng.Assert;
+	import org.testng.annotations.Parameters;
+	import org.testng.annotations.Test;
 	
 	public class sample_Demo {
 	
+		@Parameters("Browser")
 		@Test
-		public void sampleMethod() {
+		public void sampleMethod(String browserName) {
 	
-			WebDriver driver = new ChromeDriver();
+			System.out.println("browserName >> " + browserName);
+			WebDriver driver = null;
+			if (browserName.contains("Chrome"))
+				driver = new ChromeDriver();
+			else if(browserName.contains("Edge"))
+				driver = new EdgeDriver();
 			driver.manage().window().maximize();
 			driver.get("https://google.com");
 			String title = driver.getTitle();
